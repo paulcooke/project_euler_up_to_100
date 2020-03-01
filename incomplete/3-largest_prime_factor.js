daa8009bf73a7c71066120f372ff13bf39ae4d06
Paul Cooke <paulgfcooke@gmail.com>
2020-03-01 20:14:31 +0000

    project euler update

---
+++ incomplete/3-largest_prime_factor.js
@@ -0,0 +1,11 @@
+const findPrime = (num) => {
+  const square = Math.ceil(Math.sqrt(num))
+  console.log(square)
+  
+
+  // get square route for max nunber to check
+  // make list of primes
+  // check this list in reverse order to see if % each === 0
+}
+
+console.log(findPrime(600851475143))
