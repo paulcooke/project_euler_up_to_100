df43536f1bad37690fb3229875b87b72a65e70f1
Paul Cooke <paulgfcooke@gmail.com>
2020-03-06 19:07:43 +0000

    project euler exploring jest

---
+++ incomplete/12-highly_divisible_triangular_number.js
@@ -0,0 +1,7 @@
+const highlyDivisible = (limit) => {
+  const arr = [1, 2]
+  return arr
+} 
+
+
+module.exports = { highlyDivisible }
---
+++ tests/12-highly_devisible_triangular_number.test.js
@@ -0,0 +1,9 @@
+const { highlyDivisible } = require('../incomplete/12-highly_divisible_triangular_number')
+
+describe('highlyDivisible', () => {
+  it('returns an array', () => {
+    // not working? works if [500]
+    // expect(500).toEqual(expect.any(Array))
+  })
+
+})
